2e9977267928c129d01935b430c2334597e7b4a4
Nick Kowalchyk <nickkowalchyk@gmail.com>
2019-08-02 22:56:26 -0400

    Started Player Controller

---
+++ LowrezHorror/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*  Written By: Nick Kowalchyk
+ *  Character Controller Class
+ * 
+ * 
+ * 
+ * 
+ */
+public class PlayerController : MonoBehaviour {
+    // Start is called before the first frame update
+    public float speed = 5.0f;  //movement speed
+    public float jumpSpeed = 8.0f;  //jump velocity
+    public float gravity = 20.0f;   //gravity acceleration
+
+    public float cameraRotationSpeedX = 2.0f;   //speed to rotate the camera horizontally
+    public float cameraRotationSpeedY = 2.0f;   //speed to rotate the camera vertically
+    private float yaw = 0.0f;   //camera rotation value horizontally
+    private float pitch = 0.0f; //camera rotation value vertically
+
+    private CharacterController characterController;    //Unity CharacterController Asset that moves the Player object
+    private Vector3 moveDirection = Vector3.zero;   //movement direction Vector3 updated every FixedUpdate determined by arrow keys
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void FixedUpdate() {
+        if(true) {//characterController.isGrounded) {
+
+            yaw += cameraRotationSpeedX * Input.GetAxis("Mouse X");
+            pitch -= cameraRotationSpeedY * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if(Input.GetButton("Jump")) {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        //moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
