935dc29eea7fdba3d99db3f2837cd5bc1b772e5f
Nick Kowalchyk <nickkowalchyk@gmail.com>
2019-08-05 23:24:20 -0400

    Fixed Player FOV

--- LowrezHorror/Assets/Scripts/PlayerController.cs
+++ LowrezHorror/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     // Start is called before the first frame update
     public float speed = 5.0f;  //movement speed
+    public float shiftSpeed = 8.0f; //sprint speed
     public float jumpSpeed = 8.0f;  //jump velocity
     public float gravity = 20.0f;   //gravity acceleration
 
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour {
     public float cameraRotationSpeedY = 2.0f;   //speed to rotate the camera vertically
     private float yaw = 0.0f;   //camera rotation value horizontally
     private float pitch = 0.0f; //camera rotation value vertically
+    private float maxYView = 50.0f;
 
     private CharacterController characterController;    //Unity CharacterController Asset that moves the Player object
     private Vector3 moveDirection = Vector3.zero;   //movement direction Vector3 updated every FixedUpdate determined by arrow keys
@@ -36,13 +38,21 @@ public class PlayerController : MonoBehaviour {
             yaw += cameraRotationSpeedX * Input.GetAxis("Mouse X");
             pitch -= cameraRotationSpeedY * Input.GetAxis("Mouse Y");
 
+            if((pitch < -maxYView) || (pitch > maxYView)) {
+                pitch = maxYView * Mathf.Sign(pitch);
+            }
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = Camera.main.transform.TransformDirection(moveDirection);
             moveDirection.y = 0.0f;
 
-            moveDirection *= speed;
+            if(Input.GetKey(KeyCode.LeftShift)) {
+                moveDirection *= shiftSpeed;
+            }
+            else {
+                moveDirection *= speed;
+            }
 
             if(Input.GetButton("Jump")) {
                 //moveDirection.y = jumpSpeed;
