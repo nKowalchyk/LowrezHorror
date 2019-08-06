8333b85a95c90857f9c786c74612e89a865c36a7
Alex McGrath <45976293+alexmmcgrath@users.noreply.github.com>
2019-08-06 00:03:28 -0400

    Revert "Added Nicks Player stuff"
    This reverts commit f15b0e8fb528c08128a5eec3958bae3c70bee5c4.

--- LowrezHorror/Assets/Scripts/PlayerController.cs
+++ LowrezHorror/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,9 @@ using UnityEngine;
  * 
  * 
  */
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     // Start is called before the first frame update
     public float speed = 5.0f;  //movement speed
-    public float shiftSpeed = 8.0f; //sprint speed
     public float jumpSpeed = 8.0f;  //jump velocity
     public float gravity = 20.0f;   //gravity acceleration
 
@@ -21,49 +19,32 @@ public class PlayerController : MonoBehaviour
     public float cameraRotationSpeedY = 2.0f;   //speed to rotate the camera vertically
     private float yaw = 0.0f;   //camera rotation value horizontally
     private float pitch = 0.0f; //camera rotation value vertically
-    private float maxYView = 50.0f;
 
     private CharacterController characterController;    //Unity CharacterController Asset that moves the Player object
     private Vector3 moveDirection = Vector3.zero;   //movement direction Vector3 updated every FixedUpdate determined by arrow keys
-    void Start()
-    {
+    void Start() {
         characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
     }
 
-    void FixedUpdate()
-    {
-        if (true)
-        {//characterController.isGrounded) {
+    void FixedUpdate() {
+        if(true) {//characterController.isGrounded) {
 
             yaw += cameraRotationSpeedX * Input.GetAxis("Mouse X");
             pitch -= cameraRotationSpeedY * Input.GetAxis("Mouse Y");
 
-            if ((pitch < -maxYView) || (pitch > maxYView))
-            {
-                pitch = maxYView * Mathf.Sign(pitch);
-            }
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = Camera.main.transform.TransformDirection(moveDirection);
             moveDirection.y = 0.0f;
 
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                moveDirection *= shiftSpeed;
-            }
-            else
-            {
-                moveDirection *= speed;
-            }
+            moveDirection *= speed;
 
-            if (Input.GetButton("Jump"))
-            {
+            if(Input.GetButton("Jump")) {
                 //moveDirection.y = jumpSpeed;
             }
         }
