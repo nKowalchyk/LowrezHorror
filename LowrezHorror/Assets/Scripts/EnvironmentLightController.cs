2b33bf4da8f4352e306d2836b4c0d7d72cf6a1e2
unknown <nickkowalchyk@gmail.com>
2019-08-11 20:26:17 -0400

    Added Light Toggling, Ghost Enemy, Reorganized Test and Prod folders

---
+++ LowrezHorror/Assets/Scripts/EnvironmentLightController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentLightController : ILightBehaviorController {
+    // Start is called before the first frame update
+    private Light[] lights;
+    public bool isEnabled;
+    void Start() {
+        lights = transform.gameObject.GetComponentsInChildren<Light>();
+        Debug.Log(lights.Length);
+        if(lights.Length > 0) {
+            isEnabled = lights[0].enabled;
+            Debug.Log(lights[0].enabled);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public override void toggleLights() {
+        foreach(var light in lights) {
+            light.enabled = !light.enabled;
+        }
+        isEnabled = !isEnabled;
+    }
+}
---
+++ LowrezHorror/Assets/Scripts/GhostController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour {
+    public enum GhostState {
+        ToggleLight, Chase, Patrol
+    }
+    private float lightRange = 1.0f;
+
+
+    public GhostState state = GhostState.Patrol;
+    public GameObject[] lights;
+    void Start() {
+        lights = GameObject.FindGameObjectsWithTag("EnvLight");
+    }
+    // Update is called once per frame
+    void Update() {
+        foreach(var light in lights) {
+            if(Vector3.Distance(transform.position, light.transform.position) < lightRange && light.GetComponent<EnvironmentLightController>().isEnabled) {
+                light.GetComponent<EnvironmentLightController>().toggleLights();
+            }
+        }
+    }
+}
